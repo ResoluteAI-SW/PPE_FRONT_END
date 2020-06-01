d5bf8e47ebdd751d40d09d70e6a72bcc6122824b
Neelaksh <neelakshsharma1997@gmail.com>
2020-06-01 22:21:07 +0530

    Logs Screen plus generating temperory logs integrated

--- ppe_frontend/src/Components/Settings/IPCameraRegistration.js
+++ ppe_frontend/src/Components/Settings/IPCameraRegistration.js
@@ -20,7 +20,7 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import { UserContext } from "../AdminDashboard";
-import Firebase, { rdb } from "../../FirebaseConfig";
+import { rdb } from "../../FirebaseConfig";
 
 const colorHashtags = ["entry", "exit", "lab", "lobby"];
 
