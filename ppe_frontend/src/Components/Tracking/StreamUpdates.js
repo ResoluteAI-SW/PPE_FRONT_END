0eefb3527c46d31b574a1fdcf11e2b8d539ec81f
Neelaksh <neelakshsharma1997@gmail.com>
2020-06-12 16:03:11 +0530

    PPE logs moved from tracking section to reports

--- ppe_frontend/src/Components/Tracking/StreamUpdates.js
+++ ppe_frontend/src/Components/Tracking/StreamUpdates.js
@@ -2,51 +2,10 @@ import React, { useEffect, useState, useContext } from "react";
 import { Button } from "@material-ui/core";
 import { rdb } from "../../FirebaseConfig";
 import { UserContext } from "../AdminDashboard";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import moment from "moment";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Firebase from "firebase";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import AnimatedProgressProvider from "./AnimatedProgressProvider";
-import { easeQuadInOut } from "d3-ease";
 import "react-circular-progressbar/dist/styles.css";
 import CustomCircularProgressbar from "./CircularProgressBar";
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-const useStyles = makeStyles({
-  table: {
-    maxWidth: 500,
-  },
-  logsTable: {
-    minWidth: 900,
-  },
-});
-
 export default function StreamUpdates(props) {
-  const classes = useStyles();
-
   const user = useContext(UserContext);
   const [totalPeople, setTotalPeople] = useState(15);
   const [bodySuit, setBodySuit] = useState(0);
@@ -55,20 +14,10 @@ export default function StreamUpdates(props) {
   const [headgear, setHeadgear] = useState(0);
   const [safetyGoggles, setSafetyGoggles] = useState(0);
   const [masks, setMasks] = useState(0);
-  const [logs, setLogs] = useState([]);
 
   useEffect(() => {
     const IPAddress = props.IPAddress.toString().replace(/\./g, "_");
-    rdb
-      .ref(`PPE_Alerts/${user.id}/${IPAddress}/Logs`)
-      .on("value", (snapshot) => {
-        setLogs([]);
-        const collection = snapshot.val();
-        for (const docID in collection) {
-          let obj = collection[docID];
-          setLogs((logs) => logs.concat(obj));
-        }
-      });
+
     rdb.ref(`PPE_Alerts/${user.id}/${IPAddress}`).on("value", (snapshot) => {
       const collection = snapshot.val();
       for (const field in collection) {
@@ -106,24 +55,6 @@ export default function StreamUpdates(props) {
     });
   }, []);
 
-  const generateLogs = () => {
-    const IPAddress = props.IPAddress.toString().replace(/\./g, "_");
-    const pushingRef = rdb
-      .ref(`PPE_Alerts/${user.id}/${IPAddress}/Logs`)
-      .push();
-    pushingRef.set({
-      people: 15,
-      body_Suit: 12,
-      boots: 8,
-      gloves: 9,
-      headgear: 6,
-      mask: 9,
-      headgear: 2,
-      safety_goggles: 15,
-      timestamp: Firebase.database.ServerValue.TIMESTAMP,
-    });
-  };
-
   return (
     <div>
       <Button
@@ -132,9 +63,6 @@ export default function StreamUpdates(props) {
         onClick={() => props.handleBack()}
       >
         Back to Dashboard
-      </Button>
-      <Button color="primary" onClick={generateLogs}>
-        Generate Logs
       </Button>
       <div
         style={{
@@ -208,43 +136,6 @@ export default function StreamUpdates(props) {
           </div>
         </div>
       </div>
-      <h1 style={{ margin: 10 }}>PPE Alerts Logs</h1>
-      <TableContainer>
-        <Table className={classes.logsTable} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Total Persons</StyledTableCell>
-              <StyledTableCell align="right">No body suit</StyledTableCell>
-              <StyledTableCell align="right">No boots</StyledTableCell>
-              <StyledTableCell align="right">No Gloves</StyledTableCell>
-              <StyledTableCell align="right">No headgear</StyledTableCell>
-              <StyledTableCell align="right">No mask</StyledTableCell>
-              <StyledTableCell align="right">Safety Goggles</StyledTableCell>
-              <StyledTableCell align="right">Timestamp</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {logs.map((row) => (
-              <StyledTableRow key={logs.id}>
-                <StyledTableCell component="th" scope="row">
-                  {row.people}
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.body_Suit}</StyledTableCell>
-                <StyledTableCell align="right">{row.boots}</StyledTableCell>
-                <StyledTableCell align="right">{row.gloves}</StyledTableCell>
-                <StyledTableCell align="right">{row.headgear}</StyledTableCell>
-                <StyledTableCell align="right">{row.mask}</StyledTableCell>
-                <StyledTableCell align="right">
-                  {row.safety_goggles}
-                </StyledTableCell>
-                <StyledTableCell align="right">
-                  {moment(row.timestamp).format("MMMM Do YYYY, h:mm:ss a")}
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
     </div>
   );
 }
